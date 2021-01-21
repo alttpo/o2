f397ccc5729bf714ab785777b709969718da2ce4
jsd1982 <james.jdunne@gmail.com>
2021-01-20 22:12:57 -0600

    Add test for BusReader

--- snes/rom_test.go
+++ snes/rom_test.go
@@ -1,20 +1,26 @@
 package snes
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"testing"
 )
 
-func TestNewROM(t *testing.T) {
+func sampleROM() []byte {
 	contents := make([]byte, 0x8000)
-	_, err := hex.Decode(
+	hex.Decode(
 		contents[0x7FB0:],
-		[]byte("018d2401e2306bffffffffffffffffff544845204c4547454e44204f46205a454c4441202020020a03010100f2500dafffffffff2c82ffff2c82c9800080d882ffffffff2c822c822c822c820080d882"),
+		[]byte("018d2401e2306bffffffffffffffffff"+
+			"544845204c4547454e44204f46205a45"+
+			"4c4441202020020a03010100f2500daf"+
+			"ffffffff2c82ffff2c82c9800080d882"+
+			"ffffffff2c822c822c822c820080d882"),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	return contents
+}
 
+func TestNewROM(t *testing.T) {
+	contents := sampleROM()
 	gotR, err := NewROM(contents)
 	if err != nil {
 		t.Fatal(err)
@@ -29,5 +35,23 @@ func TestNewROM(t *testing.T) {
 	}
 	if gotR.NativeVectors.NMI != 0x80c9 {
 		t.Fatal("NativeVectors.NMI")
+	}
+}
+
+func TestROM_BusReader(t *testing.T) {
+	contents := sampleROM()
+	rom, err := NewROM(contents)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r := rom.BusReader(0x00FFEA)
+	p := uint16(0)
+	err = binary.Read(r, binary.LittleEndian, &p)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if p != 0x80c9 {
+		t.Fatal("expected NMI vector at $FFEA")
 	}
 }
