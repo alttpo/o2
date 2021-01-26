2a4be25d836ca5aea1d062a980dacc3bab247cda
jsd1982 <james.jdunne@gmail.com>
2021-01-25 22:58:59 -0600

    Show time since last read. Getting large gaps of 83ms about every 280ms.

--- game/alttp/game.go
+++ game/alttp/game.go
@@ -1,8 +1,10 @@
 package alttp
 
 import (
+	"log"
 	"o2/snes"
 	"strings"
+	"time"
 )
 
 // implements game.Game
@@ -29,22 +31,33 @@ func (g *Game) Description() string {
 	return strings.TrimRight(string(g.rom.Header.Title[:]), " ")
 }
 
-func (g *Game) Start() {
+func (g *Game) Load() {
 	if rc, ok := g.conn.(snes.ROMControl); ok {
 		path, cmds := rc.MakeUploadROMCommands(g.rom.Name, g.rom.Contents)
 		g.conn.EnqueueMulti(cmds)
 		g.conn.EnqueueMulti(rc.MakeBootROMCommands(path))
 	}
+}
 
-	g.conn.EnqueueMulti(
-		g.conn.MakeReadCommands([]snes.ReadRequest{
-			{
-				Address:   0xF50010,
-				Size:      0xF0,
-				Completed: nil,
+func (g *Game) Start() {
+	var lastQueued time.Time
+	var cmdReadMain snes.CommandSequence
+	cmdReadMain = g.conn.MakeReadCommands([]snes.ReadRequest{
+		{
+			Address: 0xF50010,
+			Size:    0xF0,
+			Completed: func(rsp snes.ReadOrWriteResponse) {
+				now := time.Now()
+				log.Printf("%v, %v\n", now.Sub(lastQueued).Microseconds(), rsp.Data[0x0A])
+				//log.Printf("\n%s\n", hex.Dump(rsp.Data))
+				lastQueued = now
+				g.conn.EnqueueMulti(cmdReadMain)
 			},
-		}),
-	)
+		},
+	})
+
+	lastQueued = time.Now()
+	g.conn.EnqueueMulti(cmdReadMain)
 }
 
 func (g *Game) Stop() <-chan struct{} {
