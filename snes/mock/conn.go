370d4484b5c1986afc1a9e356b22d6088e650e89
jsd1982 <james.jdunne@gmail.com>
2021-01-26 19:36:12 -0600

    Mock device simulates reads and writes

--- snes/mock/conn.go
+++ snes/mock/conn.go
@@ -1,15 +1,62 @@
 package mock
 
-import "o2/snes"
+import (
+	"o2/snes"
+	"time"
+)
 
 type Conn struct {
 	snes.BaseConn
 }
 
 func (c *Conn) MakeReadCommands(reqs []snes.ReadRequest) snes.CommandSequence {
-	panic("implement me")
+	seq := make(snes.CommandSequence, 0, len(reqs))
+	for _, req := range reqs {
+		seq = append(seq, &readCommand{req})
+	}
+	return seq
 }
 
 func (c *Conn) MakeWriteCommands(reqs []snes.WriteRequest) snes.CommandSequence {
-	panic("implement me")
+	seq := make(snes.CommandSequence, 0, len(reqs))
+	for _, req := range reqs {
+		seq = append(seq, &writeCommand{req})
+	}
+	return seq
+}
+
+type readCommand struct {
+	Request snes.ReadRequest
+}
+
+func (r *readCommand) Execute(conn snes.Conn) error {
+	<-time.After(time.Millisecond*2)
+	completed := r.Request.Completed
+	if completed != nil {
+		completed(snes.ReadOrWriteResponse{
+			IsWrite: false,
+			Address: r.Request.Address,
+			Size:    r.Request.Size,
+			Data:    make([]byte, r.Request.Size),
+		})
+	}
+	return nil
+}
+
+type writeCommand struct {
+	Request snes.WriteRequest
+}
+
+func (r *writeCommand) Execute(conn snes.Conn) error {
+	<-time.After(time.Millisecond*2)
+	completed := r.Request.Completed
+	if completed != nil {
+		completed(snes.ReadOrWriteResponse{
+			IsWrite: true,
+			Address: r.Request.Address,
+			Size:    r.Request.Size,
+			Data:    r.Request.Data,
+		})
+	}
+	return nil
 }
