6f6769872a4c52e672cfd20a65c5dd55a472cf63
jsd1982 <james.jdunne@gmail.com>
2021-01-29 11:47:32 -0600

    web: fix up go:generate snafu with debug tags

--- webui/o2/embed.go
+++ webui/o2/embed.go
@@ -1,5 +1,4 @@
-//+build !debug
-
 package main
 
-//go:generate go-bindata -fs -nomemcopy -prefix ../static ../static
+//go:generate go-bindata -fs -nomemcopy -o bindata.go -tags !debug -prefix ../static ../static
+//go:generate go-bindata -fs -nomemcopy -o bindata_debug.go -tags debug -debug -prefix ../static ../static
--- webui/o2/embed_debug.go
+++
@@ -1,3 +0,0 @@
-package main
-
-//go:generate go-bindata -debug -fs -nomemcopy -prefix ../static ../static
