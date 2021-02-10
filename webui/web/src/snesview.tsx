a83c54b60239a435cd1fa1f0963790a1da12703d
jsd1982 <james.jdunne@gmail.com>
2021-02-10 00:05:33 -0600

    web: disable components for drivers that are not connected

--- webui/web/src/snesview.tsx
+++ webui/web/src/snesview.tsx
@@ -24,21 +24,28 @@ export default ({ch, snes}: SNESProps) => {
         if (drv.isConnected) {
             return <button type="button" onClick={cmdDisconnect.bind(this, drv)}>Disconnect</button>;
         } else {
-            return <button type="button" onClick={cmdConnect.bind(this, drv)}>Connect</button>;
+            return <button type="button" disabled={snes.isConnected && !drv.isConnected}
+                           onClick={cmdConnect.bind(this, drv)}>Connect</button>;
         }
     };
 
-    return <div>
-        {(snes.drivers || []).map(drv => <Fragment key={drv.name}>
-            <h4>{drv.displayName}</h4>
-            <h5>{drv.displayDescription}</h5>
-            <label for="device">Device</label>
-            <select id="device" onChange={(e) => setDeviceIndex(e.currentTarget.selectedIndex)}>
-                <option>(Select a SNES Device)</option>
-                {(drv.devices || []).map((dev, i) =>
-                    <option selected={(i+1) == drv.selectedDevice}>{dev}</option>)}
-            </select>
-            {connectButton(drv)}
-        </Fragment>)}
-    </div>;
+    return <Fragment>
+        {(snes.drivers || []).map(drv => {
+            const {name} = drv;
+            return <div class="card" key={name}>
+                <h4>{drv.displayName}</h4>
+                <h5>{drv.displayDescription}</h5>
+                <label for={`device-${name}`}>Device</label>
+                <select
+                    disabled={snes.isConnected && !drv.isConnected}
+                    id={`device-${name}`}
+                    onChange={(e) => setDeviceIndex(e.currentTarget.selectedIndex)}>
+                    <option>(Select a SNES Device)</option>
+                    {(drv.devices || []).map((dev, i) =>
+                        <option selected={(i + 1) == drv.selectedDevice}>{dev}</option>)}
+                </select>
+                {connectButton(drv)}
+            </div>;
+        })}
+    </Fragment>;
 };
