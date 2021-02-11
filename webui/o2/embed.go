0fec00f568efd303ac072cdf479e8d8e73802eaf
jsd1982 <james.jdunne@gmail.com>
2021-02-11 16:33:07 -0600

    web: move webroot content out to dist/ folder

--- webui/o2/embed.go
+++ webui/o2/embed.go
@@ -1,4 +1,4 @@
 package main
 
-//go:generate go-bindata -fs -nomemcopy -o bindata.go       -tags !debug        -ignore /\. -prefix ../content/webroot ../content/webroot/ ../content/webroot/r/
-//go:generate go-bindata -fs -nomemcopy -o bindata_debug.go -tags  debug -debug -ignore /\. -prefix ../content/webroot ../content/webroot/ ../content/webroot/r/
+//go:generate go-bindata -fs -nomemcopy -o bindata.go       -tags !debug        -ignore /\. -prefix ../dist ../dist/ ../dist/r/
+//go:generate go-bindata -fs -nomemcopy -o bindata_debug.go -tags  debug -debug -ignore /\. -prefix ../dist ../dist/ ../dist/r/
