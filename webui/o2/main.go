2661236ec65ca618c66d57bbd11377da42957129
jsd1982 <james.jdunne@gmail.com>
2021-02-15 13:50:10 -0600

    web: organize main and comment imports

--- webui/o2/main.go
+++ webui/o2/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 	"github.com/skratchdot/open-golang/open"
-	"io/ioutil"
 	"log"
 	"net"
 	"o2/engine"
-	"o2/snes"
-	"o2/snes/fxpakpro"
-	_ "o2/snes/mock"
 	"os"
 	"strconv"
-	"sync"
+)
+
+// include these SNES drivers:
+import (
+	_ "o2/snes/fxpakpro"
+	_ "o2/snes/mock"
+)
+
+// include these game providers:
+import (
+	_ "o2/games"
+	_ "o2/games/alttp"
 )
 
 var (
@@ -77,71 +83,4 @@ func openWebUI() {
 	if err != nil {
 		log.Println(err)
 	}
-}
-
-func test() {
-	conn, err := snes.Open("fxpakpro", fxpakpro.DeviceDescriptor{})
-	if err != nil {
-		log.Printf("%v\n", err)
-		quitSystray()
-		return
-	}
-
-	if rc, ok := conn.(snes.ROMControl); ok {
-		rom, err := ioutil.ReadFile("lttpj.smc")
-		if err != nil {
-			log.Printf("%v\n", err)
-			quitSystray()
-			return
-		}
-		path, seq := rc.MakeUploadROMCommands("lttp.smc", rom)
-		conn.EnqueueMulti(seq)
-		seq = rc.MakeBootROMCommands(path)
-		conn.EnqueueMulti(seq)
-	}
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	conn.EnqueueMulti(conn.MakeReadCommands([]snes.ReadRequest{
-		{
-			Address: 0x007FC0,
-			Size:    0x40,
-			Completed: func(b snes.ReadOrWriteResponse) {
-				fmt.Printf("read  %06x %02x\n%s\n", b.Address, b.Size, hex.Dump(b.Data))
-				wg.Done()
-			},
-		},
-	}))
-	wg.Wait()
-
-	wg = sync.WaitGroup{}
-	wg.Add(1)
-	conn.EnqueueMulti(conn.MakeWriteCommands([]snes.WriteRequest{
-		{
-			Address: 0x007FEA, // NMI vector in bank 00
-			Size:    2,
-			Data:    []byte{0xC9, 0x80},
-			Completed: func(b snes.ReadOrWriteResponse) {
-				fmt.Printf("write %06x %02x\n%s\n", b.Address, b.Size, hex.Dump(b.Data))
-				wg.Done()
-			},
-		},
-	}))
-	wg.Wait()
-
-	wg = sync.WaitGroup{}
-	wg.Add(1)
-	conn.EnqueueMulti(conn.MakeReadCommands([]snes.ReadRequest{
-		{
-			Address: 0x007FC0,
-			Size:    0x40,
-			Completed: func(b snes.ReadOrWriteResponse) {
-				fmt.Printf("read  %06x %02x\n%s\n", b.Address, b.Size, hex.Dump(b.Data))
-				wg.Done()
-			},
-		},
-	}))
-	wg.Wait()
-
-	conn.Enqueue(&snes.CloseCommand{})
 }
---
+++ webui/o2/main_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/hex"
+	"io/ioutil"
+	"log"
+	"o2/snes"
+	"o2/snes/fxpakpro"
+	"sync"
+)
+
+func test() {
+	conn, err := snes.Open("fxpakpro", fxpakpro.DeviceDescriptor{})
+	if err != nil {
+		log.Printf("%v\n", err)
+		quitSystray()
+		return
+	}
+
+	if rc, ok := conn.(snes.ROMControl); ok {
+		rom, err := ioutil.ReadFile("lttpj.smc")
+		if err != nil {
+			log.Printf("%v\n", err)
+			quitSystray()
+			return
+		}
+		path, seq := rc.MakeUploadROMCommands("lttp.smc", rom)
+		conn.EnqueueMulti(seq)
+		seq = rc.MakeBootROMCommands(path)
+		conn.EnqueueMulti(seq)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	conn.EnqueueMulti(conn.MakeReadCommands([]snes.ReadRequest{
+		{
+			Address: 0x007FC0,
+			Size:    0x40,
+			Completed: func(b snes.ReadOrWriteResponse) {
+				fmt.Printf("read  %06x %02x\n%s\n", b.Address, b.Size, hex.Dump(b.Data))
+				wg.Done()
+			},
+		},
+	}))
+	wg.Wait()
+
+	wg = sync.WaitGroup{}
+	wg.Add(1)
+	conn.EnqueueMulti(conn.MakeWriteCommands([]snes.WriteRequest{
+		{
+			Address: 0x007FEA, // NMI vector in bank 00
+			Size:    2,
+			Data:    []byte{0xC9, 0x80},
+			Completed: func(b snes.ReadOrWriteResponse) {
+				fmt.Printf("write %06x %02x\n%s\n", b.Address, b.Size, hex.Dump(b.Data))
+				wg.Done()
+			},
+		},
+	}))
+	wg.Wait()
+
+	wg = sync.WaitGroup{}
+	wg.Add(1)
+	conn.EnqueueMulti(conn.MakeReadCommands([]snes.ReadRequest{
+		{
+			Address: 0x007FC0,
+			Size:    0x40,
+			Completed: func(b snes.ReadOrWriteResponse) {
+				fmt.Printf("read  %06x %02x\n%s\n", b.Address, b.Size, hex.Dump(b.Data))
+				wg.Done()
+			},
+		},
+	}))
+	wg.Wait()
+
+	conn.Enqueue(&snes.CloseCommand{})
+}
