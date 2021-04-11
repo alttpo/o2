4c5b5e2957490676fbc6c8fb47ab8e274de91540
jsd1982 <james.jdunne@gmail.com>
2021-04-10 19:58:33 -0500

    game: corrected SNES to FX Pak Pro address translation for SRAM; updating WRAM now works!

--- games/alttp/run.go
+++ games/alttp/run.go
@@ -345,7 +345,7 @@ func (g *Game) updateWRAM() {
 
 	// calculate target address in FX Pak Pro address space:
 	// SRAM starts at $E00000
-	target := (targetSNES - 0x700000) + 0xE00000
+	target := xlatSNEStoPak(targetSNES)
 	g.nextUpdateA = !g.nextUpdateA
 
 	// write generated asm routine to SRAM:
@@ -361,9 +361,9 @@ func (g *Game) updateWRAM() {
 			{
 				// JSR $7C00 | JSR $7E00
 				// update the $7C or $7E byte in the JSR instruction:
-				Address: (preMainAddr + 2 - 0x700000) + 0xE00000,
+				Address: xlatSNEStoPak(preMainAddr + 2),
 				Size:    1,
-				Data:    []byte{uint8(target >> 8)},
+				Data:    []byte{uint8(targetSNES >> 8)},
 			},
 		},
 		func(cmd snes.Command, err error) {
@@ -374,4 +374,24 @@ func (g *Game) updateWRAM() {
 			g.updateInProgress = false
 		},
 	).EnqueueTo(g.queue)
+}
+
+func snesBankToLinear(addr uint32) uint32 {
+	bank := addr >> 16
+	linbank := ((bank & 1) << 15) + ((bank >> 1) << 16)
+	linoffs := linbank + (addr & 0x7FFF)
+	return linoffs
+}
+
+func xlatSNEStoPak(snes uint32) uint32 {
+	if snes & 0x8000 == 0 {
+		if snes >= 0x700000 && snes < 0x7E0000 {
+			sram := snesBankToLinear(snes - 0x700000) + 0xE00000
+			return sram
+		} else if snes >= 0x7E0000 && snes < 0x800000 {
+			wram := (snes - 0x7E0000) + 0xE50000
+			return wram
+		}
+	}
+	return snes
 }
