e487c71e293d676073ba9dc21b871827d8656b1e
jsd1982 <james.jdunne@gmail.com>
2021-05-01 22:55:36 -0500

    interfaces: build out default implementations of Observer and Observable

--- interfaces/observable.go
+++ interfaces/observable.go
@@ -9,4 +9,35 @@ type Observable interface {
 	Unsubscribe(observer Observer)
 }
 
-type ObserverList map[Observer]Observer
+type ObserverList []Observer
+
+type ObservableImpl struct {
+	observers ObserverList
+}
+
+func (o *ObservableImpl) Subscribe(observer Observer) {
+	o.observers = append(o.observers, observer)
+}
+
+func (o *ObservableImpl) Unsubscribe(observer Observer) {
+	if o.observers == nil {
+		return
+	}
+	for i := len(o.observers); i > 0; i-- {
+		if o.observers[i] == observer {
+			o.observers = append(o.observers[0:i], o.observers[i+1:]...)
+		}
+	}
+}
+
+func (o *ObservableImpl) Publish(object interface{}) {
+	for _, observer := range o.observers {
+		observer.Notify(object)
+	}
+}
+
+type ObserverImpl func(object interface{})
+
+func (o ObserverImpl) Notify(object interface{}) {
+	o(object)
+}
