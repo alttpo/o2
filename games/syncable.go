6257050ae40f95490a6caa56274e3b8747e188d6
James Dunne <james.jdunne@gmail.com>
2022-01-23 17:22:27 -0600

    alttp: fix write confirmation/failure ASM generation for SyncableBitU16

--- games/syncable.go
+++ games/syncable.go
@@ -173,12 +173,14 @@ func (s *SyncableBitU8) GenerateUpdate(a *asm.Emitter, index uint32) bool {
 	}
 
 	// write confirmation:
+	a.Label(fmt.Sprintf("write confirmation for #%d:", index))
 	a.LDA_imm8_b(0x01)
 	a.STA_long(a.GetBase() + 0x02 + index)
 	a.BRA(nextLabel)
 
 	a.Label(failLabel)
 	// write failure:
+	a.Label(fmt.Sprintf("write failure for #%d:", index))
 	a.LDA_imm8_b(0x00)
 	a.STA_long(a.GetBase() + 0x02 + index)
 
@@ -325,14 +327,20 @@ func (s *SyncableBitU16) GenerateUpdate(a *asm.Emitter, index uint32) bool {
 	}
 
 	// write confirmation:
+	a.Label(fmt.Sprintf("write confirmation for #%d:", index))
+	a.SEP(0x30)
 	a.LDA_imm8_b(0x01)
 	a.STA_long(a.GetBase() + 0x02 + index)
+	a.REP(0x30)
 	a.BRA(nextLabel)
 
 	a.Label(failLabel)
 	// write failure:
+	a.Label(fmt.Sprintf("write failure for #%d:", index))
+	a.SEP(0x30)
 	a.LDA_imm8_b(0x00)
 	a.STA_long(a.GetBase() + 0x02 + index)
+	a.REP(0x30)
 
 	a.Label(nextLabel)
 
@@ -454,12 +462,14 @@ func (s *SyncableMaxU8) GenerateUpdate(a *asm.Emitter, index uint32) bool {
 	}
 
 	// write confirmation:
+	a.Label(fmt.Sprintf("write confirmation for #%d:", index))
 	a.LDA_imm8_b(0x01)
 	a.STA_long(a.GetBase() + 0x02 + index)
 	a.BRA(nextLabel)
 
 	a.Label(failLabel)
 	// write failure:
+	a.Label(fmt.Sprintf("write failure for #%d:", index))
 	a.LDA_imm8_b(0x00)
 	a.STA_long(a.GetBase() + 0x02 + index)
 
