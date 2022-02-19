71e4008d9315338fbad47a8a42c1a272008b2546
Jim Dunne <Jim_Dunne@ultimatesoftware.com>
2022-02-19 09:39:32 -0600

    linux: fix to not immediately exit after starting up

--- webui/o2/tray_linux.go
+++ webui/o2/tray_linux.go
@@ -7,6 +7,8 @@ import (
 func createSystray() {
 	// just open the browser UI on startup:
 	openWebUI()
+	// sleep the main goroutine so the process does not exit immediately:
+	select{}
 }
 
 func quitSystray() {
